51a1e21820622613e9b45b08116d634d75efecab
zach <zach.r.baker1@gmail.com>
2018-02-14 07:02:14 -0800

    images and will change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,25 @@ import s from './index.module.css'
 
 const IndexPage = () => (
   <div>
-    <h1 className={s.middleTitle}>word.</h1>
+    <h1 className={s.middleTitle}>story.</h1>
 
     <div className={s.work}>
-      <div className={s.workChild}>
+      <div className={`${s.workChild} ${s.bio}`}>
         <div className={s.section}>
           <h3 className={s.sectionTitle}>bio</h3>
         </div>
       </div>
-      <div className={s.workChild}>
+      <div className={`${s.workChild} ${s.tv}`}>
         <div className={s.section}>
           <h3 className={s.sectionTitle}>tv</h3>
         </div>
       </div>
-      <div className={s.workChild}>
+      <div className={`${s.workChild} ${s.stage}`}>
         <div className={s.section}>
           <h3 className={s.sectionTitle}>stage</h3>
         </div>
       </div>
-      <div className={s.workChild}>
+      <div className={`${s.workChild} ${s.teach}`}>
         <div className={s.section}>
           <h3 className={s.sectionTitle}>teach</h3>
         </div>
